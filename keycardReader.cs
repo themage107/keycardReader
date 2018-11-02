cb6e908ebfa73ab19b659425e0d031ef23edff7b
Adrian J. Fox <themage107@gmail.com>
2018-11-01 17:03:00 -0700

    Read the keycard and unlock the door
    Unlock door, lock door if character dies or level restarts. Also beep
    and change emission maps.

---
+++ keycardReader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keycardReader : MonoBehaviour {
+
+    public BoxCollider2D readerCollider;
+    public GameObject readerGO;
+    public Texture redLight;
+    public Texture greenLight;
+    public bool doorUnlocked;
+
+    public GameObject keycard;
+    keycard keycardVars;
+
+    void Start()
+    {
+        readerGO.GetComponent<Renderer>().material.SetTexture("_EmissionMap", redLight);
+        keycardVars = keycard.GetComponent<keycard>();
+        doorUnlocked = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "JohnnyHitBox")
+        {
+            if (keycardVars.keyCardInPoss)
+            {
+                readerGO.GetComponent<Renderer>().material.SetTexture("_EmissionMap", greenLight);
+                this.gameObject.GetComponent<AudioSource>().Play();
+                readerCollider.enabled = false;
+                doorUnlocked = true;
+            }
+        }
+    }
+ 
+
+}
